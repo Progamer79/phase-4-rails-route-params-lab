6b3fa11d82c220bba54134cd6a3fc0af53faa0a4
Progamer79 <cboyles66@yahoo.com>
2021-08-29 19:42:19 -0400

    Done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,37 @@
+# The official solution was credited below:
+
 class StudentsController < ApplicationController
 
-  def index
+  # The below method passed the non-bonus test:
+  def index_0
     students = Student.all
+    render json: students
+  end
+
+  def grades 
+    students = Student.order(grade: :desc)
+    render json: students
+  end
+  
+  def highest_grade 
+    student = Student.order(grade: :desc).limit(1)
+    render json: student
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  # The method below is from the official solution and it 
+  # works for the bonus test as well as for the 
+  # non-bonus test:
+  def index
+    students = if params[:name]
+                 Student.by_name(params[:name])
+               else
+                 Student.all
+               end    
     render json: students
   end
 
